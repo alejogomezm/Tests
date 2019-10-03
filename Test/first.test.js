4367057bae4a2b73768b420eff8df4eee2e6e27e
Alejandro Gomez <alejandro.gomez@realpage.com>
2019-10-03 16:56:32 -0500

    second commit

--- Test/first.test.js
+++ Test/first.test.js
@@ -1,2 +1,20 @@
-fixture `Login Test`
-    .page `https://google.com`
+import { Selector } from 'testcafe';
+
+const signButton = Selector('.login')
+const email = Selector('#email')
+const password = Selector('#passwd')
+const login = Selector("#SubmitLogin")
+
+
+fixture `Sign in Test`
+    .page `http://automationpractice.com/index.php`
+
+test('New test 1', async t => {
+    await t
+        .click(signButton)
+        .takeScreenshot()
+        .typeText(email,"testcafeClass@test.com")
+        .typeText(password,"Test@1234")
+        .click(login)
+        .takeScreenshot()
+});
